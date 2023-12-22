3f5711e95975139ef4f38793fbfe04f156c84065
Joakim Hedlund <contact@joakimhedlund.com>
2023-12-22 00:13:38 +0000

    fix: more example orders

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import { emitter } from './src/emitter'
 
 await (async () => {
-  // Emit events
+  // Here we are emitting hardcoded events but in practise they would be coming from a message queue
+
   await emitter.emit('shipped', { userId: 3252536, orderId: 19427492 }) // red bull & LEGO
-  // await emitter.emit('order', { userId: 3252536, orderId: 19366786 }) // ssd drive
+  await emitter.emit('order', { userId: 3252536, orderId: 19366786 }) // ssd drive
+  await emitter.emit('shipped', { userId: 3252536, orderId: 18471523 }) // 04:56
+  await emitter.emit('shipped', { userId: 3252536, orderId: 17826160 }) // 05:00
 })()
   .catch((err) => {
     console.error(err)
