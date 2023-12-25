b85df4b1688f65427492729396e892b38498316c
Joakim Hedlund <contact@joakimhedlund.com>
2023-12-26 00:26:09 +0100

    feat(cheevos): Frequent Flyer

---
+++ src/achievements/78-frequent-flyer.ts
@@ -0,0 +1,21 @@
+import { type AchievementMeta } from '../models/achievement'
+import { type Predicates, makeListeners } from './_rowShippedListener'
+
+export const meta = {
+  id: 78,
+  name: 'Frequent Flyer',
+  level: 6,
+  isSecret: false,
+  description: 'Hämtat ordrar i 10 st butiker',
+  experiencePoints: 10000,
+  imageName: 'frequent_flyer',
+} satisfies AchievementMeta
+
+export const predicates = {} satisfies Predicates
+
+export const listeners = makeListeners({
+  meta,
+  predicates,
+  counter: 'store',
+  required: 10,
+})
