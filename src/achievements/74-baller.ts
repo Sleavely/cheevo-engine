916a6ea633c8c420532ca350bad7aa987c324b00
Joakim Hedlund <contact@joakimhedlund.com>
2023-12-25 23:51:13 +0100

    feat(cheevos): Baller

---
+++ src/achievements/74-baller.ts
@@ -0,0 +1,21 @@
+import { type AchievementMeta } from '../models/achievement'
+import { type Predicates, makeListeners } from './_rowShippedListener'
+
+export const meta = {
+  id: 74,
+  name: 'Baller',
+  level: 1,
+  isSecret: false,
+  description: 'Handlat för över 100000 kr',
+  experiencePoints: 5000,
+  imageName: 'baller',
+}satisfies AchievementMeta
+
+export const predicates = {} satisfies Predicates
+
+export const listeners = makeListeners({
+  meta,
+  predicates,
+  counter: 'totalSum',
+  required: 100000,
+})
