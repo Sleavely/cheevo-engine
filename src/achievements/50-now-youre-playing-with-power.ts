fd86000b9a9ffabc9ad2b25c229147ab59774c78
Joakim Hedlund <contact@joakimhedlund.com>
2023-12-26 00:22:20 +0100

    feat(cheevos): Now You're Playing With Power

---
+++ src/achievements/50-now-youre-playing-with-power.ts
@@ -0,0 +1,23 @@
+import { type AchievementMeta } from '../models/achievement'
+import { type Predicates, makeListeners } from './_rowShippedListener'
+
+export const meta = {
+  id: 50,
+  name: "Now You're Playing with Power",
+  level: 0,
+  isSecret: false,
+  description: 'Köpt en Webhallen Config-dator',
+  experiencePoints: 5000,
+  imageName: 'now_youre_playing_with_power',
+} satisfies AchievementMeta
+
+export const predicates = {
+  row: (row) => row.product.name.includes('Webhallen Config'),
+} satisfies Predicates
+
+export const listeners = makeListeners({
+  meta,
+  predicates,
+  counter: 'quantity',
+  required: 1,
+})
