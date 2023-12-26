3e978b28f314f20cb9b5edfc8da9cb4425280f7a
Joakim Hedlund <contact@joakimhedlund.com>
2023-12-26 17:34:34 +0100

    feat(cheevos): Fudge You

---
+++ src/achievements/1317-fudge-you.ts
@@ -0,0 +1,28 @@
+import { type AchievementMeta } from '../models/achievement'
+import { type Predicates, makeListeners } from './_rowShippedListener'
+import { isWithinRecurring } from '../lib/temporal'
+
+export const meta = {
+  id: 1317,
+  name: 'Fudge You',
+  level: 1,
+  isSecret: false,
+  description: 'Lagt en order den 28 december. Linus Torvalds födelsedag (1969)',
+  experiencePoints: 1000,
+  imageName: 'fudge_you',
+} satisfies AchievementMeta
+
+export const predicates = {
+  order: (order) => isWithinRecurring({
+    evaluand: order.orderDate,
+    startConstraints: { year: 1969, month: 12, day: 28 },
+    endConstraints: { month: 12, day: 28 },
+  }),
+} satisfies Predicates
+
+export const listeners = makeListeners({
+  meta,
+  predicates,
+  counter: 'order',
+  required: 1,
+})
