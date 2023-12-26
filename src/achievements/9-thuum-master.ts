d04d31f0f9eca7fc281b07dec9d1d02f2808f5ec
Joakim Hedlund <contact@joakimhedlund.com>
2023-12-26 08:32:05 +0100

    feat(cheevos): Thu'um Master

---
+++ src/achievements/9-thuum-master.ts
@@ -0,0 +1,21 @@
+import { type AchievementMeta } from '../models/achievement'
+import { type Predicates, makeListeners } from './_rowShippedListener'
+
+export const meta = {
+  id: 9,
+  name: "Thu'um Master",
+  level: 5,
+  isSecret: false,
+  description: '100 hämtade/skickade ordrar',
+  experiencePoints: 5000,
+  imageName: 'thuum_master',
+} satisfies AchievementMeta
+
+export const predicates = {} satisfies Predicates
+
+export const listeners = makeListeners({
+  meta,
+  predicates,
+  counter: 'order',
+  required: 100,
+})
