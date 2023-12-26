8ab6b36257367dbd933b3a3cb8d2afef6e83b295
Joakim Hedlund <contact@joakimhedlund.com>
2023-12-26 08:29:47 +0100

    feat(cheevos): Stockholm Syndrome

---
+++ src/achievements/8-stockholm-syndrome.ts
@@ -0,0 +1,24 @@
+import { type AchievementMeta } from '../models/achievement'
+import { type Predicates, makeListeners } from './_rowShippedListener'
+
+export const meta = {
+  id: 8,
+  name: 'Stockholm Syndrome',
+  level: 0,
+  isSecret: false,
+  description: 'Hämtat ordrar i samtliga Stockholms-butiker',
+  experiencePoints: 10000,
+  imageName: 'stockholm_syndrome',
+} satisfies AchievementMeta
+
+const storesIds = [1, 2, 9, 11, 14, 15, 16, 19, 20, 28, 21]
+export const predicates = {
+  order: (order) => order.store !== null && storesIds.includes(order.store.id),
+} satisfies Predicates
+
+export const listeners = makeListeners({
+  meta,
+  predicates,
+  counter: 'store',
+  required: storesIds.length,
+})
