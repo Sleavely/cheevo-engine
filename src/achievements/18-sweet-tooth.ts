50efce6d8d3c179075bde7fe98cbe7ca73bf8d9a
Joakim Hedlund <contact@joakimhedlund.com>
2023-12-26 18:40:03 +0100

    feat(cheevos): Sweet Tooth

---
+++ src/achievements/18-sweet-tooth.ts
@@ -0,0 +1,28 @@
+import { type AchievementMeta } from '../models/achievement'
+import { type Predicates, makeListeners } from './_rowShippedListener'
+
+export const meta = {
+  id: 18,
+  name: 'Sweet Tooth',
+  level: 0,
+  isSecret: false,
+  description: 'Köpt godis för 1000 kr i en order',
+  experiencePoints: 2500,
+  imageName: 'sweet_tooth',
+} satisfies AchievementMeta
+
+export const predicates = {
+  order: (order) => {
+    return order.rows.reduce((sum, row) => {
+      if (!row.product.categoryTree.includes('/Godis/')) return sum
+      return sum + (row.unitPrice * row.quantity)
+    }, 0) >= 1000
+  },
+} satisfies Predicates
+
+export const listeners = makeListeners({
+  meta,
+  predicates,
+  counter: 'order',
+  required: 1,
+})
