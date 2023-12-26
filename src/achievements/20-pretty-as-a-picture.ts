d988947cae3a574b41a85621640517776bb1d21c
Joakim Hedlund <contact@joakimhedlund.com>
2023-12-26 18:40:21 +0100

    feat(cheevos): Pretty As A Picture

---
+++ src/achievements/20-pretty-as-a-picture.ts
@@ -0,0 +1,23 @@
+import { type AchievementMeta } from '../models/achievement'
+import { type Predicates, makeListeners } from './_rowShippedListener'
+
+export const meta = {
+  id: 20,
+  name: 'Pretty as a Picture',
+  level: 1,
+  isSecret: false,
+  description: 'Köpt personvård för 2500 kr',
+  experiencePoints: 2500,
+  imageName: 'pretty_as_a_picture',
+} satisfies AchievementMeta
+
+export const predicates = {
+  row: (row) => row.product.categoryTree.includes('Personvård'),
+} satisfies Predicates
+
+export const listeners = makeListeners({
+  meta,
+  predicates,
+  counter: 'price',
+  required: 2500,
+})
