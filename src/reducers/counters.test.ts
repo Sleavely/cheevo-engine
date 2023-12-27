97aff1d465ac34bb07588cb5d136094ab69f3430
Joakim Hedlund <contact@joakimhedlund.com>
2023-12-27 18:59:56 +0100

    refactor: use Orders fixtures

--- src/reducers/counters.test.ts
+++ src/reducers/counters.test.ts
@@ -1,92 +1,31 @@
 import { describe, test, expect } from 'vitest'
-import { type Order, type OrderRow } from '../models/orders'
+import * as fixtures from '../models/orders.fixtures'
 import { reduceOrder, reduceOrders, reduceRow } from './counters'
-
-const storeTemplate = {
-  id: 1,
-  isPickupPoint: true,
-  location: { lat: 1, lng: 1 },
-  zipCode: '15257',
-  address: 'Halldalen 1 B',
-  name: 'Anstalten Hall',
-  city: 'Södertälje',
-} satisfies Order['store']
-
-const orderTemplate = {
-  id: 1337,
-  orderDate: 19900107,
-  readyDate: 19900107,
-  sentDate: 19900107,
-  totalSum: 30,
-  reservedUntil: null,
-  paymentMethod: { id: 1, name: 'Vitest Bank', isPrepayment: false, isRefundedAutomatically: false },
-  shippingAddress: { name: 'AInders', address: 'Buren', zipCode: '11111', city: 'Stockholm' },
-  shippingMethod: { id: 1, name: 'Nanobot delivery', price: 5, vat: 1 },
-  currency: 'SEK',
-  trackingNumber: null,
-  trackingUrl: null,
-  statusCode: 200,
-  discount: 0,
-  discountCodes: [],
-  isPrivateCustomer: true,
-  deliveries: [],
-  rows: [],
-  store: {
-    ...storeTemplate,
-  },
-  productKeys: [],
-  isExtendable: false,
-  userExperiencePointBoosts: [],
-  cjEvent: null,
-} satisfies Order
-
-const rowTemplate = {
-  id: 1,
-  unitPrice: 1,
-  unitVat: 0.20,
-  quantity: 1,
-  product: {
-    id: 1,
-    name: 'GeForce 9090',
-    release: { timestamp: 19900107, format: '' },
-    thumbnail: '',
-    longDeliveryNotice: null,
-    customerWorkshopInfo: { text: null, url: null },
-    categoryTree: '',
-    section: { id: 1, metaTitle: '', name: '', icon: '', active: true },
-    manufacturer: { id: 1, name: '', visible: true, websiteUrl: '', takeoverUrl: null },
-  },
-  locked: true,
-  subscriptionData: [],
-  insuranceInfo: null,
-  hasSubscription: false,
-  sentDate: 19900107,
-} satisfies OrderRow
 
 describe('reduceRow', () => {
   test('returns quantity if no predicate', () => {
-    const row = { ...rowTemplate, quantity: 25 }
+    const row = { ...fixtures.orderRow(), quantity: 25 }
 
     const result = reduceRow('quantity', row)
 
     expect(result).toBe(25)
   })
   test('returns quantity if predicate evaluates to true', () => {
-    const row = { ...rowTemplate, quantity: 25 }
+    const row = { ...fixtures.orderRow(), quantity: 25 }
 
     const result = reduceRow('quantity', row, (r) => r.quantity === 25)
 
     expect(result).toBe(25)
   })
   test('returns 0 if predicate evaluates to false', () => {
-    const row = { ...rowTemplate, quantity: 25 }
+    const row = { ...fixtures.orderRow(), quantity: 25 }
 
     const result = reduceRow('quantity', row, (r) => r.id === 749)
 
     expect(result).toBe(0)
   })
   test('price counter returns sum', () => {
-    const row = { ...rowTemplate, quantity: 12, unitPrice: 2 }
+    const row = { ...fixtures.orderRow(), quantity: 12, unitPrice: 2 }
 
     const result = reduceRow('price', row)
 
@@ -96,21 +35,21 @@ describe('reduceRow', () => {
 
 describe('reduceOrder', () => {
   test('returns total quantity if no predicates', () => {
-    const order = { ...orderTemplate, rows: [{ ...rowTemplate, quantity: 2 }, { ...rowTemplate, quantity: 3 }] }
+    const order = { ...fixtures.order(), rows: [{ ...fixtures.orderRow(), quantity: 2 }, { ...fixtures.orderRow(), quantity: 3 }] }
 
     const result = reduceOrder('quantity', order)
 
     expect(result).toBe(5)
   })
   test('returns 0 if order predicate evaluates to false', () => {
-    const order = { ...orderTemplate, rows: [{ ...rowTemplate, quantity: 87 }] }
+    const order = { ...fixtures.order(), rows: [{ ...fixtures.orderRow(), quantity: 87 }] }
 
     const result = reduceOrder('quantity', order, { order: () => false })
 
     expect(result).toBe(0)
   })
   test('only counts rows matching row predicate', () => {
-    const order = { ...orderTemplate, rows: [{ ...rowTemplate, id: 15, quantity: 87 }, { ...rowTemplate, id: 9, quantity: 4 }] }
+    const order = { ...fixtures.order(), rows: [{ ...fixtures.orderRow(), id: 15, quantity: 87 }, { ...fixtures.orderRow(), id: 9, quantity: 4 }] }
 
     const result = reduceOrder('quantity', order, { row: (r) => r.id === 9 })
 
@@ -121,8 +60,8 @@ describe('reduceOrder', () => {
 describe('reduceOrders', () => {
   test('returns total quantity if no predicates', () => {
     const orders = [
-      { ...orderTemplate, rows: [{ ...rowTemplate, quantity: 4 }, { ...rowTemplate }] },
-      { ...orderTemplate, rows: [{ ...rowTemplate, quantity: 12 }] },
+      { ...fixtures.order(), rows: [{ ...fixtures.orderRow(), quantity: 4 }, { ...fixtures.orderRow() }] },
+      { ...fixtures.order(), rows: [{ ...fixtures.orderRow(), quantity: 12 }] },
     ]
 
     const result = reduceOrders('quantity', orders)
@@ -131,9 +70,9 @@ describe('reduceOrders', () => {
   })
   test('only counts orders and rows matching predicate', () => {
     const orders = [
-      { ...orderTemplate, id: 1, rows: [{ ...rowTemplate, quantity: 1 }] },
-      { ...orderTemplate, id: 2, rows: [{ ...rowTemplate, id: 15, quantity: 87 }, { ...rowTemplate, id: 9, quantity: 4 }] },
-      { ...orderTemplate, id: 3, rows: [{ ...rowTemplate, quantity: 1 }] },
+      { ...fixtures.order(), id: 1, rows: [{ ...fixtures.orderRow(), quantity: 1 }] },
+      { ...fixtures.order(), id: 2, rows: [{ ...fixtures.orderRow(), id: 15, quantity: 87 }, { ...fixtures.orderRow(), id: 9, quantity: 4 }] },
+      { ...fixtures.order(), id: 3, rows: [{ ...fixtures.orderRow(), quantity: 1 }] },
     ]
 
     const result = reduceOrders('quantity', orders, {
@@ -145,8 +84,8 @@ describe('reduceOrders', () => {
   })
   test('can sum the total ordersums (including shipping and whatnot)', () => {
     const orders = [
-      { ...orderTemplate, totalSum: 50000 },
-      { ...orderTemplate, totalSum: 25000 },
+      { ...fixtures.order(), totalSum: 50000 },
+      { ...fixtures.order(), totalSum: 25000 },
     ]
 
     const ballerProgress = reduceOrders('totalSum', orders)
@@ -156,15 +95,15 @@ describe('reduceOrders', () => {
   test('can count unique stores matching a predicate', () => {
     const orders = [
       // will be ignored by order predicate
-      { ...orderTemplate, id: 1, rows: [{ ...rowTemplate, quantity: 1 }] },
+      { ...fixtures.order(), id: 1, rows: [{ ...fixtures.orderRow(), quantity: 1 }] },
       // will be ignored by row predicate
-      { ...orderTemplate, id: 2, rows: [{ ...rowTemplate, id: 15, quantity: 87 }, { ...rowTemplate, id: 9, quantity: 4 }] },
+      { ...fixtures.order(), id: 2, rows: [{ ...fixtures.orderRow(), id: 15, quantity: 87 }, { ...fixtures.orderRow(), id: 9, quantity: 4 }] },
       // +1
-      { ...orderTemplate, id: 3, rows: [{ ...rowTemplate, quantity: 1 }], store: { ...storeTemplate, id: 1 } },
+      { ...fixtures.order(), id: 3, rows: [{ ...fixtures.orderRow(), quantity: 1 }], store: { ...fixtures.store(), id: 1 } },
       // +1
-      { ...orderTemplate, id: 4, rows: [{ ...rowTemplate, quantity: 1 }], store: { ...storeTemplate, id: 2 } },
+      { ...fixtures.order(), id: 4, rows: [{ ...fixtures.orderRow(), quantity: 1 }], store: { ...fixtures.store(), id: 2 } },
       // duplicate store
-      { ...orderTemplate, id: 5, rows: [{ ...rowTemplate, quantity: 1 }], store: { ...storeTemplate, id: 2 } },
+      { ...fixtures.order(), id: 5, rows: [{ ...fixtures.orderRow(), quantity: 1 }], store: { ...fixtures.store(), id: 2 } },
     ]
     const uniqueStores = reduceOrders('store', orders, {
       order: (o) => o.id > 1,
